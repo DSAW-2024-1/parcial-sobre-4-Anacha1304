e300f3c7dd519c1b89be4369b0eef6bb4e4c9b06
Ana Chaves <anachpe@unisabana.edu.co>
2024-02-22 23:47:07 -0500

    Entrega

--- Funcionalidad.js
+++ Funcionalidad.js
@@ -1,12 +1,36 @@
-// Obtener referencia al botón y al área oculta
-let showHiddenSectionButton = document.getElementById("q");
+
+/*let showHiddenSectionButton = document.getElementById("q");
 let hiddenSection = document.getElementById("r");
 
-// Agregar evento click al botón
+
 showHiddenSectionButton.addEventListener("click", function() {
-    // Mostrar el área oculta cambiando su clase
     hiddenSection.classList.remove("hidden");
-    // Desplazar la página hasta el área mostrada
     hiddenSection.scrollIntoView({ behavior: "smooth" });
-});
+});*/
+
+// Función para mostrar la sección "Success-tab"
+function showSuccessTab() {
+    let successTab = document.getElementById("Success-tab");
+    successTab.classList.remove("hidden");
+}
+
+function dhowSuccessTab() {
+    let mainSection = document.getElementById("mainSection");
+    let successTab = document.getElementById("Success-tab");
+    
+    mainSection.classList.remove("hidden");
+    successTab.classList.add("hidden");
+}
+// Función para ocultar la sección "Success-tab"
+function hideSuccessTab() {
+    let successTab = document.getElementById("Success-tab");
+    successTab.classList.add("hidden");
+}
+
+// Obtener referencia al botón "Success" y agregar evento click
+document.getElementById("Success").addEventListener("click", showSuccessTab);
+
+// Obtener referencia al botón "Dismiss" y agregar evento click
+document.getElementById("Dismiss").addEventListener("click", hideSuccessTab);
+
 
